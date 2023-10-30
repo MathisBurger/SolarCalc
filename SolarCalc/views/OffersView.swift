3705634da73187f3fe0b862eaa054e702ae6809e
Mathis Burger <handymathis@gmail.com>
2023-10-30 12:04:07 +0100

    [TASK] Added base data to offers view

--- SolarCalc/views/OffersView.swift
+++ SolarCalc/views/OffersView.swift
@@ -12,10 +12,39 @@ struct OffersView: View {
     
     var request: CalculationRequest?;
     
+    @State private var response: OffersResponse? = nil;
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-            .onAppear() {
-                print(request)
+        ScrollView {
+            if (self.response == nil) {
+                ProgressView()
+            } else {
+                VStack(alignment: .leading) {
+                    Text(String(format: "Jährlicher Ertrag: %.2f", self.response!.yearly_yield))
+                    Text(String(format:"Autarkie: %.2f", self.response!.autarky))
+                    Text(String(format: "CO2 Einsparung: %.2f", self.response!.co2_savings))
+                    Text(String(format: "Anlagenfläche: %.2f", self.response!.total_plant_surface))
+                }.padding()
+                    .frame(maxWidth: .infinity)
+                    .background(Color(.sRGB, red: 9/255, green: 111/255, blue: 182/255, opacity: 0.1))
+                .cornerRadius(10)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(Color(.sRGB, red: 150/255, green: 150/255, blue: 150/255, opacity: 0.1), lineWidth: 1)
+                )
+                .padding([.top, .horizontal])
             }
+        }
+        .onAppear() {
+            if (self.request != nil) {
+                do {
+                    try ApiService.getSolarOffers(request: self.request!) {res in
+                        self.response = res;
+                    }
+                } catch {
+                    print(error)
+                }
+            }
+        }
     }
 }
